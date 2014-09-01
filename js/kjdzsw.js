44f02e30dd884172d940015c7303ef70761651c3
qianzhangsheng <qianzhangsheng@yeah.net>
2014-09-01 14:26:33 +0800

    Initial commit

---
+++ js/kjdzsw.js
@@ -0,0 +1,14 @@
+(function ($) {
+    $("#qs_01").on("click", function () {
+        $(".an_27").show();
+    });
+    $("#qs_02").on("click", function () {
+        $(".an_27").hide();
+    });
+    $("#qs_03").on("click", function () {
+        $(".an_913").show();
+    });
+    $("#qs_04").on("click", function () {
+        $(".an_913").hide();
+    });
+})(jQuery);
